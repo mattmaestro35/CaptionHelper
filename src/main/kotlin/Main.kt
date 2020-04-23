29fb992e19f45d81fa7acff56f32a4c10f70b588
mattmaestro35 <mlarson7@illinois.edu>
2020-04-23 14:58:58 -0500

    Added image blur code.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,83 +1,181 @@
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
-import javax.swing.text.html.ImageView
+import kotlin.math.roundToInt
 
-
-import java.awt.Color
-import java.awt.Graphics
-import java.awt.image.BufferedImage
-import java.lang.Thread.sleep
-
-/*Class and some main code taken from
-https://rosettacode.org/wiki/Bitmap/PPM_conversion_through_a_pipe#Kotlin
-To enable working with a Bitmap, since this isn't an Android project and 
-so I don't get BitmapFactory.
- */
-class BasicBitmapStorage(width: Int, height: Int) {
-    val image = BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR)
-
-    fun fill(c: Color) {
-        val g = image.graphics
-        g.color = c
-        g.fillRect(0, 0, image.width, image.height)
-    }
-
-    fun setPixel(x: Int, y: Int, c: Color) = image.setRGB(x, y, c.getRGB())
-
-    fun getPixel(x: Int, y: Int) = Color(image.getRGB(x, y))
-}
 
 fun main(args: Array<String>) {
-    // create BasicBitmapStorage object
-    val width = 640
-    val height = 640
-    val bbs = BasicBitmapStorage(width, height)
-    val theImage: BufferedImage = ImageIO.read(File("testImage.png"))
-    for (w in 0..(width - 1)) {
-        for (h in 0..(height - 1)) {
-            val theColor = theImage.getRGB(w, h)
-            println(theColor)
+
+    /* Image convolution code modified from https://rosettacode.org/wiki/Image_convolution */
+    class ArrayData(val width: Int, val height: Int) {
+        var dataArray = IntArray(width * height)
+
+        operator fun get(x: Int, y: Int) = dataArray[y * width + x]
+
+        operator fun set(x: Int, y: Int, value: Int) {
+            dataArray[y * width + x] = value
         }
     }
-    //get pixel colors from theImage
 
+    fun bound(value: Int, endIndex: Int) = when {
+        value < 0        -> 0
+        value < endIndex -> value
+        else             -> endIndex - 1
+    }
 
-    //set pixel colors in bbs
+    fun convolute(
+        inputData: ArrayData,
+        kernel: ArrayData,
+        kernelDivisor: Int
+    ): ArrayData {
+        val inputWidth = inputData.width
+        val inputHeight = inputData.height
+        val kernelWidth = kernel.width
+        val kernelHeight = kernel.height
+        if (kernelWidth <= 0 || (kernelWidth and 1) != 1)
+            throw IllegalArgumentException("Kernel must have odd width")
+        if (kernelHeight <= 0 || (kernelHeight and 1) != 1)
+            throw IllegalArgumentException("Kernel must have odd height")
+        val kernelWidthRadius = kernelWidth ushr 1
+        val kernelHeightRadius = kernelHeight ushr 1
+
+        val outputData = ArrayData(inputWidth, inputHeight)
+        for (i in inputWidth - 1 downTo 0) {
+            for (j in inputHeight - 1 downTo 0) {
+                var newValue = 0.0
+                for (kw in kernelWidth - 1 downTo 0) {
+                    for (kh in kernelHeight - 1 downTo 0) {
+                        newValue += kernel[kw, kh] * inputData[
+                                bound(i + kw - kernelWidthRadius, inputWidth),
+                                bound(j + kh - kernelHeightRadius, inputHeight)
+                        ].toDouble()
+                        outputData[i, j] = (newValue / kernelDivisor).roundToInt().toInt()
+                    }
+                }
+            }
+        }
+        return outputData
+    }
+
+    fun getArrayDatasFromImage(filename: String): Array<ArrayData> {
+        val inputImage = ImageIO.read(File(filename))
+        //val outputfile = File("doubleCheck.png")
+        //ImageIO.write(inputImage, "png", outputfile)
+        val width = inputImage.width
+        val height = inputImage.height
+        val rgbData = inputImage.getRGB(0, 0, width, height, null, 0, width)
+        val reds = ArrayData(width, height)
+        val greens = ArrayData(width, height)
+        val blues = ArrayData(width, height)
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                val rgbValue = rgbData[y * width + x]
+                reds[x, y] = (rgbValue ushr 16) and 0xFF
+                greens[x,y] = (rgbValue ushr 8) and 0xFF
+                blues[x, y] = rgbValue and 0xFF
+            }
+        }
+        return arrayOf(reds, greens, blues)
+    }
+
+    fun writeOutputImage(filename: String, redGreenBlue: Array<ArrayData>) {
+        val (reds, greens, blues) = redGreenBlue
+        val outputImage = BufferedImage(
+            reds.width, reds.height, BufferedImage.TYPE_INT_ARGB
+        )
+        for (y in 0 until reds.height) {
+            for (x in 0 until reds.width) {
+                val red = bound(reds[x , y], 256)
+                val green = bound(greens[x , y], 256)
+                val blue = bound(blues[x, y], 256)
+                outputImage.setRGB(
+                    x, y, (red shl 16) or (green shl 8) or blue or -0x01000000
+                )
+            }
+        }
+        ImageIO.write(outputImage, "PNG", File(filename))
+    }
+
+    val kernelWidth = 7
+    val kernelHeight = 7
+    val kernel = ArrayData(kernelWidth, kernelHeight)
+    val kernelArray = arrayOf(
+        2, 2, 2, 2, 2, 2, 2,
+        2, 2, 2, 2, 2, 2, 2,
+        2, 2, 3, 4, 3, 2, 2,
+        2, 2, 4, 4, 4, 2, 2,
+        2, 2, 3, 4, 3, 2, 2,
+        2, 2, 2, 2, 2, 2, 2,
+        2, 2, 2, 2, 2, 2, 2
+    )
+    val kernelDivisor = kernelArray.sum()
+    println("Kernel size: $kernelWidth x $kernelHeight, divisor = $kernelDivisor")
+
+    var y = 0
+    for (i in 0 until kernelHeight) {
+        print("[")
+        for (j in 0 until kernelWidth) {
+            kernel[j, i] = kernelArray[y++]
+            print(" ${kernel[j, i]} ")
+        }
+        println("]")
+    }
+
+    fun blur(filenameString: String, x: Int) {
+        val dataArrays = getArrayDatasFromImage(filenameString)
+        for (i in dataArrays.indices) {
+            dataArrays[i] = convolute(dataArrays[i], kernel, kernelDivisor)
+        }
+        writeOutputImage("newImage.png", dataArrays)
+
+        val dataArraysNew = getArrayDatasFromImage("newImage.png")
+        for (value in 0..x) {
+            println("Blur Iteration " + value)
+            for (i in dataArraysNew.indices) {
+                dataArraysNew[i] = convolute(dataArraysNew[i], kernel, kernelDivisor)
+            }
+        }
+        writeOutputImage("newImage.png", dataArraysNew)
+    }
+
+    blur("testImage.png", 50)
+    println("Done.")
 
 
     /*
-    for (y in 0 until height) {
-        for (x in 0 until width) {
-            val c = Color(x % 256, y % 256, (x * y) % 256)
-            bbs.setPixel(x, y, c)
+    //TODO: Add nice error handling, try/catch
+    val theImage: BufferedImage = ImageIO.read(File("testImage.png"))
+    val width = theImage.width
+    val height = theImage.height
+    val newImage: BufferedImage = BufferedImage(width, height + 100, BufferedImage.TYPE_3BYTE_BGR)
+
+
+    for (w in 0..(width - 1)) {
+        for (h in 0..(height - 1)) {
+            val rgb = theImage.getRGB(w, h)
+            val alpha: Int = rgb shr 24 and 0xFF
+            val red: Int = rgb shr 16 and 0xFF
+            val green: Int = rgb shr 8 and 0xFF
+            val blue: Int = rgb and 0xFF
+
+
+            val rgbBlurred = 1
+            newImage.setRGB(w,h, rgb)
+            //Thanks to a very helpful person on stackoverflow for how to get r,g,b from a TYPE_INT_ARGB
+            //https://stackoverflow.com/questions/6001211/format-of-type-int-rgb-and-type-int-argb
         }
     }
 
-    // now write the object in PPM format to ImageMagick's STDIN via a pipe
-    // so it can be converted to a .jpg file and written to disk
-    val pb = ProcessBuilder("convert", "-", "output_piped.jpg")
-    pb.directory(null)
-    pb.redirectInput(ProcessBuilder.Redirect.PIPE)
-    val buffer = ByteArray(width * 3) // write one line at a time
-    val proc = pb.start()
-    val pStdIn = proc.outputStream
-    pStdIn.use {
-        val header = "P6\n$width $height\n255\n".toByteArray()
-        with (it) {
-            write(header)
-            for (y in 0 until height) {
-                for (x in 0 until width) {
-                    val c = bbs.getPixel(x, y)
-                    buffer[x * 3] = c.red.toByte()
-                    buffer[x * 3 + 1] = c.green.toByte()
-                    buffer[x * 3 + 2] = c.blue.toByte()
-                }
-                write(buffer)
-            }
+
+    //TODO: Make some halfway variables for readability
+    for (w in 0 .. (width - 1)) {
+        for (h in (height..(height + 100 - 1))) {
+           // newImage.setRGB(w, h, )
         }
     }
-    sleep(1000)
+
+    val outputFile = File("newImage.png")
+    //ImageIO.write(newImage, "png", outputFile)
     */
-
 
 }
